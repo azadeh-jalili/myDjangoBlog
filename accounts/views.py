a99ce96886f2cc6a8b1e4edf62b6493ba7ff045f
azadeh-jalili <azadeh.jalili62@gmail.com>
2021-08-26 16:25:00 +0430

    log in & signup user

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -8,7 +9,8 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('myblog:list')
     else:
         form = UserCreationForm()
@@ -19,7 +21,8 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # form.save()
+            user = form.get_user()
+            login(request, user)
             return redirect('myblog:list')
     else:
         form = AuthenticationForm()
