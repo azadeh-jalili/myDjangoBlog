2c5004970982105b7989ecfb3612608d151c57bc
azadeh-jalili <azadeh.jalili62@gmail.com>
2021-08-26 10:05:25 +0430

    lesson 13

--- myblog/views.py
+++ myblog/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 from . import models
 
 
 # Create your views here.
 
 def article_list(request):
-    articles = models.article.objects.all().order_by('date')
-    args = {'articles':articles}
-    return render(request, 'myblog/myblog.html' , args)
+    articles = models.article.objects.all().order_by('-date')
+    args = {'articles': articles}
+    return render(request, 'myblog/myblog.html', args)
 
 
+def article_detail(request, slug):
+    # return HttpResponse(slug)
+    article = models.article.objects.get(slug=slug)
+    return render(request, 'myblog/article_detail.html', {'article': article})
